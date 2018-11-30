a864ab22e5172dd2ab096725a4ff2faf6e55fc10
varshadhamal <31721343+varshadhamal@users.noreply.github.com>
2018-11-30 16:46:36 +0530

    Update App.java

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World by feature_two!" );
+        System.out.println( "Hello World by bugfix!" );
     }
 }
